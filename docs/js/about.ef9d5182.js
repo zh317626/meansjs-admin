c883ad8765be7c68c7c281602a1a26293bff93c8
雾恋 <zhuhong317626@163.com>
2023-01-04 18:22:15 +0800

    :art: fix

--- docs/js/about.ef9d5182.js
+++ docs/js/about.ef9d5182.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkmeansjs_admin"]=self["webpackChunkmeansjs_admin"]||[]).push([[443],{4335:function(n,a,s){s.r(a),s.d(a,{default:function(){return f}});var e=s(3396);const u={class:"about"},t=(0,e._)("h1",null,"This is an about page",-1),r=[t];function i(n,a){return(0,e.wg)(),(0,e.iD)("div",u,r)}var c=s(89);const o={},d=(0,c.Z)(o,[["render",i]]);var f=d}}]);
-//# sourceMappingURL=about.9c18398c.js.map
+//# sourceMappingURL=about.ef9d5182.js.map
